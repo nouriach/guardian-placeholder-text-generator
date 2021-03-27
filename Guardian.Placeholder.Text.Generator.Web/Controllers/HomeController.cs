983ee7869cf50ea85ef88d2d4186d35f04099635
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-27 15:14:31 +0000

    Add Clean HTML Regex method and readonly value

--- Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
+++ Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Guardian.Placeholder.Text.Generator.Web.Controllers
@@ -13,6 +14,7 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static readonly string _htmlRegex = "[<][^<>]*[>]";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -28,6 +30,16 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
         [HttpPost]
         public IActionResult Index(CharacterSubmission value)
         {
+            // 1. Select random number to act as the 'page' number
+            // 2. Make API call to all adding random page number
+            // 3. Store all results into a match JSON model
+            // 4. This will result in a List<JSONModel> holding 10 values
+            // 5. Select a random entry from this collection
+            // 6. grab the apiLink for that value
+            // 7. Use AngleSharp to webscrape this page
+            // 8. After grabbing and storing all <p> elements clean them all with the Regex
+            // 9. Add each entry from the cleaned copy array until length matches the initial character count value
+
             return View();
         }
 
@@ -41,5 +53,12 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public static string RemoveHtmlFromCopy(string innerHtml)
+        {
+            string cleanedCopy = Regex.Replace(innerHtml, _htmlRegex, "").Trim();
+            return cleanedCopy;
+        }
+
     }
 }
