9bcf45d7c17ff9e3bf7574dfeae63a2597152efc
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-27 14:18:31 +0000

    build project

---
+++ Guardian.Placeholder.Text.Generator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Guardian.Placeholder.Text.Generator.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
