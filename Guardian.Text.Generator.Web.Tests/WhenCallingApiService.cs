03d19922101962d7e93cd181ecd4b8e9ba0188d3
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-27 15:14:54 +0000

    Write out all Unit Tests in advance

---
+++ Guardian.Text.Generator.Web.Tests/WhenCallingApiService.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardian.Text.Generator.Web.Tests
+{
+    public class WhenCallingApiService
+    {
+        [Test]
+        public static void And_Selecting_Random_Page_Number()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Test]
+        public static void And_Sending_Request_With_Random_Page_Number()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Test]
+        public static void And_Storing_Results_In_Model_Collection()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Test]
+        public static void And_Selecting_Random_Entry_From_Model_Collection()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
---
+++ Guardian.Text.Generator.Web.Tests/WhenWebScraping.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace Guardian.Text.Generator.Web.Tests
+{
+    public class WhenWebScraping
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void And_Values_Are_Stored_In_A_Collection()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        public void And_Values_Are_Cleaned_Of_Any_Html()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
