2c9b53b60faba8a64a58190a791d2c6da4135da4
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-27 15:00:15 +0000

    Create simple model to store character count

---
+++ Guardian.Placeholder.Text.Generator.Web/Models/CharacterSubmission.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardian.Text.Generator.Web.Models
+{
+    public class CharacterSubmission
+    {
+        public string Count { get; set; }
+    }
+}
