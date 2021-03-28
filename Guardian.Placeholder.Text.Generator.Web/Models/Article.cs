5df15d01123401b550ca99b88f9f94d9f4d119e6
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-28 15:26:51 +0100

    Create model for json response

---
+++ Guardian.Placeholder.Text.Generator.Web/Models/Article.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardian.Text.Generator.Web.Models
+{
+
+    public class Rootobject
+    {
+        public Response response { get; set; }
+    }
+
+    public class Response
+    {
+        public string status { get; set; }
+        public string userTier { get; set; }
+        public int total { get; set; }
+        public int startIndex { get; set; }
+        public int pageSize { get; set; }
+        public int currentPage { get; set; }
+        public int pages { get; set; }
+        public string orderBy { get; set; }
+        public Article[] results { get; set; }
+    }
+
+    public class Article
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string sectionId { get; set; }
+        public string sectionName { get; set; }
+        public DateTime webPublicationDate { get; set; }
+        public string webTitle { get; set; }
+        public string webUrl { get; set; }
+        public string apiUrl { get; set; }
+        public Tag[] tags { get; set; }
+        public bool isHosted { get; set; }
+        public string pillarId { get; set; }
+        public string pillarName { get; set; }
+    }
+    public class Tag
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string webTitle { get; set; }
+        public string webUrl { get; set; }
+        public string apiUrl { get; set; }
+        public object[] references { get; set; }
+        public string bio { get; set; }
+    }
+}
