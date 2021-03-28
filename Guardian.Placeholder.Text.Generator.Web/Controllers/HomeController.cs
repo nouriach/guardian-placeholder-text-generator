d8a5d1c85507a4810a29b35ca0235a7ea127a049
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-28 15:27:44 +0100

    Add method for calling and storing json response

--- Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
+++ Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
@@ -3,10 +3,12 @@ using Guardian.Placeholder.Text.Generator.Web.Models;
 using Guardian.Text.Generator.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -17,11 +19,16 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
         private readonly ILogger<HomeController> _logger;
         private static readonly string _htmlRegex = "[<][^<>]*[>]";
         public static int _page;
+        public static Rootobject _articles;
+        public static Article _article;
+
+        private static string _apiKey = "api-key=0ff89a23-392e-4b15-ad61-da8b70a6abd1";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+
 
         [HttpGet]
         public IActionResult Index()
@@ -32,8 +39,8 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
         [HttpPost]
         public IActionResult Index(CharacterSubmission value)
         {
-            // 1. Select random number to act as the 'page' number
-            // 2. Make API call to all adding random page number
+            // 1. Select random number to act as the 'page' number DONE
+            // 2. Make API call to all adding random page number DONE
             // 3. Store all results into a match JSON model
             // 4. This will result in a List<JSONModel> holding 10 values
             // 5. Select a random entry from this collection
@@ -42,7 +49,19 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
             // 8. After grabbing and storing all <p> elements clean them all with the Regex
             // 9. Add each entry from the cleaned copy array until length matches the initial character count value
 
+
             return View();
+        }
+
+        public static void SelectRandomSingleArticleFromCollection()
+        {
+            Random rand = new Random();
+            _article = _articles.response.results[rand.Next(0, _articles.response.results.Length)];
+        }
+
+        public static void MapJsonToArticleModel(string content)
+        {
+            _articles = JsonConvert.DeserializeObject<Rootobject>(content);
         }
 
         public IActionResult Privacy()
@@ -55,7 +74,13 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
+        public static async Task<string> SendGuardianRequest()
+        {
+            var url = $"https://content.guardianapis.com/search?section=football&page={_page}&show-tags=contributor&{_apiKey}";
+            HttpClient client = new HttpClient();
+            var result = await client.GetStringAsync(url);
+            return result;
+        }
         private static async Task<List<string>> GetPageData(string url)
         {
             List<string> results = new List<string>();
