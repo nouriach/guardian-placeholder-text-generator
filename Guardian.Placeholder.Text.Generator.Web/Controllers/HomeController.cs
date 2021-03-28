c21d4144b65fc60d29afe568625cb5f53d4aeb17
nouriach <49981579+nouriach@users.noreply.github.com>
2021-03-28 16:56:33 +0100

    Abstract method for checking HTML presence

--- Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
+++ Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
@@ -17,12 +17,16 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
         private static readonly string _htmlRegex = "[<][^<>]*[>]";
+
         public static int _page;
         public static Rootobject _articles;
         public static Article _article;
 
-        private static string _apiKey = "api-key=0ff89a23-392e-4b15-ad61-da8b70a6abd1";
+        public static List<string> _copy;
+
+        public static string _apiKey = "api-key=0ff89a23-392e-4b15-ad61-da8b70a6abd1";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -31,8 +35,9 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
 
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var result = await GetPageData("https://www.theguardian.com/football/2021/mar/27/harry-kane-shuts-out-noise-to-fix-focus-pivotal-summer-england-euros");
             return View();
         }
 
@@ -41,15 +46,18 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
         {
             // 1. Select random number to act as the 'page' number DONE
             // 2. Make API call to all adding random page number DONE
-            // 3. Store all results into a match JSON model
-            // 4. This will result in a List<JSONModel> holding 10 values
-            // 5. Select a random entry from this collection
+            // 3. Store all results into a match JSON model DONE
+            // 4. This will result in a List<JSONModel> holding 10 values DONE
+            // 5. Select a random entry from this collection DONE
             // 6. grab the apiLink for that value
             // 7. Use AngleSharp to webscrape this page
             // 8. After grabbing and storing all <p> elements clean them all with the Regex
             // 9. Add each entry from the cleaned copy array until length matches the initial character count value
-
-
+            
+            return View();
+        }
+        public IActionResult Privacy()
+        {
             return View();
         }
 
@@ -64,26 +72,22 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
             _articles = JsonConvert.DeserializeObject<Rootobject>(content);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public static async Task<string> SendGuardianRequest()
+        public static async Task<string> SendGuardianRequest(string url)
         {
-            var url = $"https://content.guardianapis.com/search?section=football&page={_page}&show-tags=contributor&{_apiKey}";
             HttpClient client = new HttpClient();
-            var result = await client.GetStringAsync(url);
+            var result = await client.GetStringAsync($"{url}&{_apiKey}");
             return result;
         }
-        private static async Task<List<string>> GetPageData(string url)
+        public static async Task<List<string>> GetPageData(string url)
         {
-            List<string> results = new List<string>();
+            _copy = new List<string>();
+
             // Load default configuration
             var config = Configuration.Default.WithDefaultLoader();
             // Create a new browsing context
@@ -95,17 +99,30 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
 
             foreach (var copy in articleCopyRows)
             {
-                if (copy.InnerHtml.Contains(_htmlRegex))
+                if (CheckIfCopyContainsHtml(copy.InnerHtml))
                 {
-                    results.Add(RemoveHtmlFromCopy(copy.InnerHtml));
+                    var result = RemoveHtmlFromCopy(copy.InnerHtml);
+                    _copy.Add(result);
                 }
                 if (!string.IsNullOrEmpty(copy.InnerHtml) && !copy.InnerHtml.Contains("modified"))
                 {
-                    results.Add(copy.InnerHtml);
+                    _copy.Add(copy.InnerHtml);
                 }
             }
 
-            return results;
+            return _copy;
+        }
+
+        public static bool CheckIfCopyContainsHtml(string copy)
+        {
+            if (Regex.IsMatch(copy, _htmlRegex))
+            {
+                return true;
+            }
+            else
+            {
+               return false;
+            }
         }
 
         public static string RemoveHtmlFromCopy(string innerHtml)
