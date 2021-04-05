f3ed9c02f814d71130cda15173d67203e38406dc
nouriach <49981579+nouriach@users.noreply.github.com>
2021-04-05 17:54:50 +0100

    Add new method for display result
    break up API call into separate properties
    
    create method to build placeholder text result

--- Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
+++ Guardian.Placeholder.Text.Generator.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using AngleSharp;
 using Guardian.Placeholder.Text.Generator.Web.Models;
 using Guardian.Text.Generator.Web.Models;
+using Guardian.Text.Generator.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -9,6 +10,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -20,13 +22,22 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
 
         private static readonly string _htmlRegex = "[<][^<>]*[>]";
 
-        public static int _page;
         public static Rootobject _articles;
         public static Article _article;
 
         public static List<string> _copy;
 
+
+        public static int _characterCountRequest;
+
+        public static string _urlBase = "https://content.guardianapis.com/search?";
+        public static int _page;
+        public static string _pageRequest = $"page=";
+        public static string _query = "q=barney-ronay";
+        public static string _queryDate = "from-date=2018-01-01";
         public static string _apiKey = "api-key=0ff89a23-392e-4b15-ad61-da8b70a6abd1";
+
+        public static string _placeholderText;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -35,55 +46,73 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var result = await GetPageData("https://www.theguardian.com/football/2021/mar/27/harry-kane-shuts-out-noise-to-fix-focus-pivotal-summer-england-euros");
+            // var result = await GetPageData("https://www.theguardian.com/football/2021/mar/27/harry-kane-shuts-out-noise-to-fix-focus-pivotal-summer-england-euros");
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(CharacterSubmission value)
+        public async Task<IActionResult> Index(CharacterSubmission value)
         {
-            // 1. Select random number to act as the 'page' number DONE
-            // 2. Make API call to all adding random page number DONE
-            // 3. Store all results into a match JSON model DONE
-            // 4. This will result in a List<JSONModel> holding 10 values DONE
-            // 5. Select a random entry from this collection DONE
-            // 6. grab the apiLink for that value
-            // 7. Use AngleSharp to webscrape this page
-            // 8. After grabbing and storing all <p> elements clean them all with the Regex
-            // 9. Add each entry from the cleaned copy array until length matches the initial character count value
-            
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
+            // Make the API Call
+            SetCharacterCountRequest(Convert.ToInt32(value.Count));
+            GetRandomPageNumber();
+            var jsonString = await SendGuardianRequest(BuildApiCall());
+            MapJsonToArticleModel(jsonString);
+            SelectRandomSingleArticleFromCollection();
+
+            // Use the result of the API call to web scrape content
+            var content = await GetPageData(_article.webUrl);
+
+            // Build the content to match the length of the character request
+            BuildPlaceHolderText(content);
+
+            // Make a new View Model
+            PlaceHolderTextResult vm = new PlaceHolderTextResult() { Content = _placeholderText };
+
+            return RedirectToAction("PlaceholderResult", vm);
         }
 
+        public IActionResult PlaceholderResult(PlaceHolderTextResult vm)
+        {
+            return View(vm);
+        }
+
+        // MAKING THE API CALL
+        public static void SetCharacterCountRequest(int count)
+        {
+            _characterCountRequest = count;
+        }
+        public static void GetRandomPageNumber()
+        {
+            Random rand = new Random();
+            _page = rand.Next(1, 10);
+        }
+        public static string BuildApiCall()
+        {
+            var url = $"{_urlBase}{_pageRequest}{_page}&{_query}&{_queryDate}&{_apiKey}";
+            return url;
+        }
+        public static async Task<string> SendGuardianRequest(string url)
+        {
+            // need to add some sort of check on the success status
+            HttpClient client = new HttpClient();
+            var result = await client.GetStringAsync(url);
+            return result;
+        }
+        public static void MapJsonToArticleModel(string content)
+        {
+            _articles = JsonConvert.DeserializeObject<Rootobject>(content);
+        }
         public static void SelectRandomSingleArticleFromCollection()
         {
             Random rand = new Random();
             _article = _articles.response.results[rand.Next(0, _articles.response.results.Length)];
         }
 
-        public static void MapJsonToArticleModel(string content)
-        {
-            _articles = JsonConvert.DeserializeObject<Rootobject>(content);
-        }
 
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public static async Task<string> SendGuardianRequest(string url)
-        {
-            HttpClient client = new HttpClient();
-            var result = await client.GetStringAsync($"{url}&{_apiKey}");
-            return result;
-        }
+        // WEBSCRAPING AN ARTICLE
         public static async Task<List<string>> GetPageData(string url)
         {
             _copy = new List<string>();
@@ -99,19 +128,16 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
 
             foreach (var copy in articleCopyRows)
             {
-                if (CheckIfCopyContainsHtml(copy.InnerHtml))
-                {
-                    var result = RemoveHtmlFromCopy(copy.InnerHtml);
-                    _copy.Add(result);
-                }
                 if (!string.IsNullOrEmpty(copy.InnerHtml) && !copy.InnerHtml.Contains("modified"))
                 {
-                    _copy.Add(copy.InnerHtml);
+                    var result = CheckIfCopyContainsHtml(copy.InnerHtml) ? RemoveHtmlFromCopy(copy.InnerHtml) : copy.InnerHtml;
+                    _copy.Add(result);
                 }
             }
 
             return _copy;
         }
+
 
         public static bool CheckIfCopyContainsHtml(string copy)
         {
@@ -131,10 +157,43 @@ namespace Guardian.Placeholder.Text.Generator.Web.Controllers
             return cleanedCopy;
         }
 
-        public static void GetRandomPageNumber()
+        // Build Placeholder Text
+        public static void BuildPlaceHolderText(List<string> result)
         {
-            Random rand = new Random();
-            _page = rand.Next(1, 100);
+            StringBuilder buildCopy = new StringBuilder();
+            int count = 0;
+
+            foreach(var sentence in result)
+            {
+                var characters = sentence.ToCharArray();
+                foreach (var ch in characters)
+                {
+                    if(count < _characterCountRequest)
+                    {
+                        count++;
+                        buildCopy.Append(ch);
+                        if (ch.ToString() == "." || ch.ToString() == "!" || ch.ToString() == "?")
+                        {
+                            buildCopy.Append(" ");
+                        }
+                    }
+                }
+            }
+            
+            _placeholderText = buildCopy.ToString();
         }
+
+        // MISC
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
     }
 }
