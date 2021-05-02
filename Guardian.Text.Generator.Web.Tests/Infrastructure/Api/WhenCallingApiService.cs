2fac53624563fe149a04e8c65e3eff8454c003d1
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-02 18:49:15 +0100

    Write Test for calling new Api Service

---
+++ Guardian.Text.Generator.Web.Tests/Infrastructure/Api/WhenCallingApiService.cs
@@ -0,0 +1,25 @@
+﻿using Guardian.Text.Generator.Web.Infrastructure.Api;
+using Guardian.Text.Generator.Web.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardian.Text.Generator.Web.Tests.Infrastructure.Api
+{
+    public class WhenCallingApiService
+    {
+        //Arrange
+        //Act
+        //Assert
+        [Test]
+        public static void And_Result_Contains_Ten_Articles()
+        {
+            //Arrange
+            //Act
+            var actual = ApiService.SendRequestAndGetArticles();
+            //Assert
+            Assert.AreEqual(actual.Result.response.results.Length, 10);
+        }
+    }
+}
