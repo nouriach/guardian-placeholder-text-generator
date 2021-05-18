1bf29f49c500d34a4c48332ef5c02529e7cfe177
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-18 15:29:07 +0100

    Create GetAllAuthors mediatr pattern

---
+++ Guardian.Placeholder.Text.Generator.Web/Application/Handlers/Authors/GetAllAuthorsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Guardian.Text.Generator.Web.Application.Interfaces;
+using Guardian.Text.Generator.Web.Application.Queries.Authors;
+using Guardian.Text.Generator.Web.Application.Results.Authors;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Guardian.Text.Generator.Web.Application.Handlers.Authors
+{
+    public class GetAllAuthorsQueryHandler : IRequestHandler<GetAuthorsQuery, GetAuthorsResult>
+    {
+        private readonly IWebscrapeService _service;
+
+        public GetAllAuthorsQueryHandler(IWebscrapeService service)
+        {
+            _service = service;
+        }
+        public async Task<GetAuthorsResult> Handle(GetAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _service.GetAllAuthorsAsync();
+            GetAuthorsResult result = new GetAuthorsResult(response);
+
+            return result;
+        }
+    }
+}
---
+++ Guardian.Placeholder.Text.Generator.Web/Application/Results/Authors/GetAuthorsResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardian.Text.Generator.Web.Application.Results.Authors
+{
+    public class GetAuthorsResult
+    {
+        public GetAuthorsResult()
+        {
+
+        }
+        public GetAuthorsResult(List<string> authors)
+        {
+            Authors = authors;
+        }
+        public List<string> Authors { get; set; }
+    }
+}
