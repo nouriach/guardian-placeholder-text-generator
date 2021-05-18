ab8c3bc0aa32b36afd69fcb58aa735d13aa897b3
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-18 15:31:12 +0100

    Create extension methods
    - remove specific characters
    - sort a list of authors alphabetically

---
+++ Guardian.Placeholder.Text.Generator.Web/Extensions/RemoveSpecificCharacters.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Guardian.Text.Generator.Web.Extensions
+{
+    public static class RemoveSpecificCharacters
+    {
+        public static string RemoveNonBreakingSpaceFromString(this String str)
+        {
+            string cleanedCopy = str.Replace("&nbsp;", " ");
+            return cleanedCopy;
+        }
+    }
+}
---
+++ Guardian.Placeholder.Text.Generator.Web/Helpers/SortCollectionIntoAlphabeticalOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Guardian.Text.Generator.Web.Helpers
+{
+    public class SortCollectionIntoAlphabeticalOrder : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x == null || y == null)
+            {
+                return 0;
+            }
+
+            // "CompareTo()" method
+            return x.CompareTo(y);
+        }
+    }
+}
