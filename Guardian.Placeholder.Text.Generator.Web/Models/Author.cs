26a6901f763cb9c88256eb077d6018f2f838c441
nouriach <49981579+nouriach@users.noreply.github.com>
2021-05-24 15:28:48 +0100

    Create new Author model

--- Guardian.Placeholder.Text.Generator.Web/Models/Author.cs
+++ Guardian.Placeholder.Text.Generator.Web/Models/Author.cs
@@ -7,5 +7,12 @@ namespace Guardian.Text.Generator.Web.Models
 {
     public class Author
     {
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        public string BylineImageUrl { get; set; }
+        public string BylineLargeImageUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Url { get; set; }
     }
 }
